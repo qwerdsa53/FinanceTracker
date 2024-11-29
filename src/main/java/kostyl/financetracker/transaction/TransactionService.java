317f982cfe12fb6c64fbf1313720b056333c4340
Andrax <andronov_maksimka@mail.ru>
2024-11-29 22:52:11 +0300

    added enum and set confugirations for requests

--- src/main/java/kostyl/financetracker/transaction/TransactionService.java
+++ src/main/java/kostyl/financetracker/transaction/TransactionService.java
@@ -1,4 +1,20 @@
 package kostyl.financetracker.transaction;
-
+import kostyl.financetracker.transaction.TransactionRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.List;
+@Service
+@AllArgsConstructor
 public class TransactionService {
+    private final TransactionRepository transactionRepository;
+    public Double getTotalAmountByUser(Long userId) {
+        return transactionRepository.getTotalAmountByUser(userId);
+    }
+    public Long getTransactionCountByDateRange(LocalDate startDate, LocalDate endDate) {
+        return transactionRepository.getTransactionCountByDateRange(startDate, endDate);
+    }
+    public List<Object[]> getTotalAmountByCategory(Long userId) {
+        return transactionRepository.getTotalAmountByCategory(userId);
+    }
 }
