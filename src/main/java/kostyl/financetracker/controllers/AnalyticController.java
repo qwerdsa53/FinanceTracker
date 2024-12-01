f91b9011e18e787ea3f9b7256122c631a070d4a9
Andrax <andronov_maksimka@mail.ru>
2024-12-01 10:42:43 +0300

    replace RequestMapping in AnalyticController

--- src/main/java/kostyl/financetracker/controllers/AnalyticController.java
+++ src/main/java/kostyl/financetracker/controllers/AnalyticController.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("api/v1/analytic")
@@ -15,83 +15,49 @@ public class AnalyticController {
 
     private final AnalyticService analyticService;
 
-    // Общая сумма транзакций пользователя
     @GetMapping("/total/{userId}")
-    public Double getTotalAmountByUser(@PathVariable Long userId) {
-        return analyticService.getTotalAmountByUser(userId);
-    }
-
-    @GetMapping("/income/{userId}")
-    public Double getTotalIncomeByUser(@PathVariable Long userId) {
-        return analyticService.getTotalIncomeByUser(userId);
-    }
-
-    @GetMapping("/expenses/{userId}")
-    public Double getTotalExpensesByUser(@PathVariable Long userId) {
-        return analyticService.getTotalExpensesByUser(userId);
-    }
-
-    @GetMapping("/balance/{userId}")
-    public Double getUserBalance(@PathVariable Long userId) { // Доходы - Расходы
-        return analyticService.getUserBalance(userId);
-    }
-
-    // Количество транзакций за период
-    @GetMapping("/count")
-    public Long getTransactionCountByDateRange(@RequestParam String startDate,
-                                               @RequestParam String endDate) {
-        return analyticService.getTransactionCountByDateRange(
-                LocalDate.parse(startDate), LocalDate.parse(endDate));
-    }
-
-    // Общая сумма по категориям
-    @GetMapping("/category/{userId}")
-    public List<CategoryStatisticsDTO> getTotalAmountByCategory(@PathVariable Long userId) {
-        return analyticService.getTotalAmountByCategory(userId);
-    }
-
-    @GetMapping("/expenses/categories/{userId}")
-    public List<CategoryStatisticsDTO> getExpensesByCategory(@PathVariable Long userId) {
-        return analyticService.getExpensesByCategory(userId);
-    }
-
-    @GetMapping("/income/categories/{userId}")
-    public List<CategoryStatisticsDTO> getIncomeByCategory(@PathVariable Long userId) {
-        return analyticService.getIncomeByCategory(userId);
-    }
-
-    @GetMapping("/categories/summary/{userId}")
-    public CategoryStatisticsDTO getCategorySummary(@PathVariable Long userId) {
-        return analyticService.getCategorySummary(userId);
-    }
-
-    // Транзакции пользователя за период
-    @GetMapping("/transactions/{userId}")
-    public List<Object[]> getTransactionsByUserAndDateRange(
+    public Dictionary<String,Object> getTotalAnalytic(
             @PathVariable Long userId,
-            @RequestParam String startDate,
-            @RequestParam String endDate) {
-        return analyticService.getTransactionsByUserAndDateRange(
-                userId, LocalDate.parse(startDate), LocalDate.parse(endDate));
+            @RequestParam(required = false) String startDate,
+            @RequestParam(required = false) String endDate) {
+
+        Dictionary<String, Object> totalAnalytic = new Hashtable<>();
+
+        // Basic data by User
+        try {
+            totalAnalytic.put("totalAmount", analyticService.getTotalAmountByUser(userId));
+        } catch (Exception e) {
+            System.out.println("AAA1");
+            e.printStackTrace();
+            totalAnalytic.put("totalAmount",0);
+        }
+        totalAnalytic.put("totalIncome", analyticService.getTotalIncomeByUser(userId));
+        totalAnalytic.put("totalExpenses", analyticService.getTotalExpensesByUser(userId));
+        totalAnalytic.put("balance", analyticService.getUserBalance(userId));
+
+        // Data by Category
+        totalAnalytic.put("totalByCategory", analyticService.getTotalAmountByCategory(userId));
+        totalAnalytic.put("expensesByCategory", analyticService.getExpensesByCategory(userId));
+        totalAnalytic.put("incomeByCategory", analyticService.getIncomeByCategory(userId));
+        try {
+            totalAnalytic.put("categorySummary", analyticService.getCategorySummary(userId));
+        } catch (Exception e) {
+            System.out.println("AAA2");
+            e.printStackTrace();
+            totalAnalytic.put("categorySummary",0);
+        }
+        // Data based on Date
+        if (startDate != null && endDate != null) {
+            LocalDate start = LocalDate.parse(startDate);
+            LocalDate end = LocalDate.parse(endDate);
+
+            totalAnalytic.put("transactionCount", analyticService.getTransactionCountByDateRange(start, end));
+            totalAnalytic.put("transactions", analyticService.getTransactionsByUserAndDateRange(userId, start, end));
+            totalAnalytic.put("expensesByDateRange", analyticService.getTotalExpensesByUserAndDateRange(userId, start, end));
+            totalAnalytic.put("categoryStatisticsByDateRange", analyticService.getCategoryStatisticsByDateRange(userId, start, end));
+        }
+
+        return totalAnalytic;
     }
 
-    // Сумма расходов пользователя за неделю/месяц
-    @GetMapping("/expenses_time/{userId}")
-    public Double getTotalExpensesByDateRange(
-            @PathVariable Long userId,
-            @RequestParam String startDate,
-            @RequestParam String endDate) {
-        return analyticService.getTotalExpensesByUserAndDateRange(
-                userId, LocalDate.parse(startDate), LocalDate.parse(endDate));
-    }
-
-    // Статистика доходов/расходов по категориям за период
-    @GetMapping("/statistics/{userId}")
-    public List<CategoryStatisticsDTO> getCategoryStatisticsByDateRange(
-            @PathVariable Long userId,
-            @RequestParam String startDate,
-            @RequestParam String endDate) {
-        return analyticService.getCategoryStatisticsByDateRange(
-                userId, LocalDate.parse(startDate), LocalDate.parse(endDate));
-    }
 }
